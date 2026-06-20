d42526c9348e6126ce2d260314c7dda8f4e053ab
matttm <matttmaloney@gmail.com>
2026-06-20 12:34:34 -0400

    finished fn, removing allocs

--- solution.go
+++ solution.go
@@ -25,5 +25,18 @@ func DecodeVaultOverride(stream []byte) (string, error) {
 	secId := (meta >> 4) & 0x00FF     // bits 4-11 => 0x65
 	encType := (meta >> 12) & 0x000F  // bits 12-15 => 0x1
 	log.Printf("clearance: %d, secId: %d, encType: %d", clearance, secId, encType)
-	return "", errors.New("TODO: implement DecodeVaultOverride")
+	plen := stream[6] // 0x08
+	if len(stream) < int(7+plen) {
+		return "", errors.New("packet too short for payload")
+	}
+	payload := stream[7 : 7+plen] // "12345678"
+	if encType == 0x1 {
+		// decrypt in place
+		for i := range payload {
+			payload[i] ^= 0x55
+		}
+	} else if encType != 0x0 {
+		return "", errors.New("unsupported encryption type")
+	}
+	return string(payload), nil
 }
